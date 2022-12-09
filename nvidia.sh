cf98b11de05e3ea0e84b7a66b24e0257bb95f6cd
aCeTotal <lars.oksendal@gmail.com>
2022-12-09 13:23:45 +0100

    Changes

--- nvidia.sh
+++ nvidia.sh
@@ -27,6 +27,7 @@ sudo dnf update -y
 sudo dnf upgrade --refresh -y
 sudo dnf install dnf-plugins-core -y
 
+#Enable RPM FUSION
 sudo dnf install \
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 
@@ -41,18 +42,35 @@ mkdir -p /home/$username/.config
 mkdir -p /home/$username/.fonts
 mkdir -p /home/$username/Dokumenter/
 mkdir -p /home/$username/Downloads/
+mkdir -p /home/$username/Bilder/
+mkdir -p /home/$username/Bilder/Screenshots/
 mkdir -p /usr/share/sddm/themes
 cp -R dotconfig/* /home/$username/.config/
 tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
 mv /home/$username/.config/sddm.conf /etc/sddm.conf
-mv /home/$username/.config/.xinitrc /home/$username/
 
 # Installing Essential Programs 
-sudo dnf install xdg-user-dirs bspwm sxhkd kitty rofi polybar picom thunar nitrogen unzip yad wget pavucontrol -y
+sudo dnf install xdg-user-dirs bspwm sxhkd kitty rofi polybar picom thunar nitrogen zip unzip mpv yad wget pavucontrol blueman network-manager-applet -y
 # Installing Other less important Programs
 sudo dnf install neofetch arandr git vim flameshot mangohud lxappearance papirus-icon-theme -y
 # Installing popular softwares
-sudo dnf install blender gimp freecad libreoffice steam discord -y
+sudo dnf install gimp freecad steam discord protonup gamemode lutris -y
+
+# Install Joplin Notebook
+sudo dnf install -y dnf-plugins-core distribution-gpg-keys -y
+sudo dnf copr enable taw/joplin
+sudo dnf install -y joplin -y
+
+#SSTP VPN, Citrix Workspace client, Outlook client, OnlyOffice + Microsoft Teams
+cd rpm/
+sudo dnf install sstp-client -y
+sudo dnf install NetworkManager-sstp -y
+sudo dnf install NetworkManager-sstp-gnome -y
+sudo dnf install rpm/ICAClient-rhel-22.12.0.12-0.x86_64.rpm -y
+sudo dnf install rpm/prospect-mail-0.4.0.x86_64.rpm -y
+sudo dnf install rpm/teams-1.5.00.23861-1.x86_64.rpm -y
+wget https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors.x86_64.rpm
+sudo dnf install onlyoffice-desktopeditors.x86_64.rpm -y && cd ..
 
 # Cursor
 sudo dnf copr enable peterwu/rendezvous -y
@@ -74,6 +92,16 @@ sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.co
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc -y
 sudo dnf install brave-browser -y
 
+# Install blender
+mkdir ~/blender-git
+mkdir ~/blender-git/lib
+cd ~/blender-git
+git clone https://git.blender.org/blender.git
+svn checkout https://svn.blender.org/svnroot/bf-blender/trunk/lib/linux_centos7_x86_64
+cd blender
+make update
+make
+
 # Download Nordic Theme
 cd /usr/share/themes/
 git clone https://github.com/EliverLara/Nordic.git
@@ -85,6 +113,9 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.z
 unzip FiraCode.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
 unzip Meslo.zip -d /home/$username/.fonts
+
+# Enable services
+
 
 # Reloading Fonts
 fc-cache -vf
