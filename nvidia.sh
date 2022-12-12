96f0f937f897519afe62c99e11dd4bfbe15558be
aCeTotal <lars.oksendal@gmail.com>
2022-12-12 01:03:55 +0100

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -10,8 +10,11 @@ username=$(id -u -n 1000)
 builddir=$(pwd)
 hostname=Fedora_Office
 
-# Enable parallell download and choose the fastest mirrors
+#Blender variables
+blenderurl=https://www.blender.org/download/release/Blender3.4/blender-3.4.0-linux-x64.tar.xz/
+blenderfile=blender-3.4.0-linux-x64
 
+# Enable parallell download and choose the fastest mirrors
 cat > /etc/dnf/dnf.conf << EOF
 [main]
 color=always
@@ -55,15 +58,14 @@ mkdir -p /home/$username/Bilder/Screenshots/
 mkdir -p /home/$username/Bilder/Wallpapers/
 mkdir -p /usr/share/sddm/themes
 cp -R dotconfig/* /home/$username/.config/
-tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
 
 #Set hostname
 sudo hostnamectl set-hostname "$hostname"
 
 # Installing Essential Programs 
-sudo dnf install xdg-user-dirs gimp freecad bspwm sxhkd xev kitty rofi polybar picom thunar nitrogen zip unzip mpv yad wget pavucontrol blueman network-manager-applet -y
+sudo dnf install xdg-user-dirs cups gnome-calculator openssh-server numlockx gimp freecad bspwm sxhkd xev kitty rofi xsetroot polybar picom thunar nitrogen zip unzip mpv yad wget pavucontrol blueman network-manager-applet -y
 # Installing Other less important Programs
-sudo dnf install neofetch prusa-slicer arandr git vim flameshot lxappearance papirus-icon-theme -y
+sudo dnf install neofetch @virtualization prusa-slicer arandr subversion mod_dav_svn git vim flameshot lxappearance papirus-icon-theme cmatrix -y
 # Installing Gaming-related stuff
 sudo dnf install steam discord protonup gamemode lutris mangohud -y
 
@@ -72,51 +74,57 @@ sudo dnf install -y dnf-plugins-core distribution-gpg-keys -y
 sudo dnf copr enable taw/joplin
 sudo dnf install -y joplin -y
 
-#SSTP VPN, Citrix Workspace client, Outlook client, OnlyOffice + Microsoft Teams
+# WORK-RELATED STUFF
+# SSTP VPN, Citrix Workspace client, Teamviewer, Outlook client, OnlyOffice + Microsoft Teams
 cd rpm/
 sudo dnf install sstp-client -y
 sudo dnf install NetworkManager-sstp -y
 sudo dnf install NetworkManager-sstp-gnome -y
 sudo dnf install rpm/ICAClient-rhel-22.12.0.12-0.x86_64.rpm -y
-sudo dnf install rpm/prospect-mail-0.4.0.x86_64.rpm -y
-sudo dnf install rpm/teams-1.5.00.23861-1.x86_64.rpm -y
+
+wget https://github.com/julian-alarcon/prospect-mail/releases/download/v0.4.0/prospect-mail-0.4.0.x86_64.rpm
+sudo dnf install prospect-mail-0.4.0.x86_64.rpm -y
+
+wget https://packages.microsoft.com/yumrepos/ms-teams/teams-1.5.00.23861-1.x86_64.rpm
+sudo dnf install teams-1.5.00.23861-1.x86_64.rpm -y
+
 wget https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors.x86_64.rpm
-sudo dnf install onlyoffice-desktopeditors.x86_64.rpm -y && cd ..
+sudo dnf install onlyoffice-desktopeditors.x86_64.rpm -y
+
+wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
+sudo dnf install teamviewer.x86_64.rpm -y
+
 
 # Cursor
 sudo dnf copr enable peterwu/rendezvous -y
 sudo dnf install bibata-cursor-themes -y
 
 # Install and enable SDDM
-sudo dnf install lightdm -y
-sudo systemctl enable lightdm
+sudo dnf install sddm -y
 sudo systemctl set-default graphical.target
 
-# Nvidia drivers
+# Nvidia drivers with CUDA
 sudo dnf update --refresh -y
 sudo dnf install akmod-nvidia -y
 sudo dnf install xorg-x11-drv-nvidia-cuda -y
 sudo dnf install xorg-x11-drv-nvidia-cuda-libs -y
 
-# Install brave
+# Install brave-browser
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ -y
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc -y
 sudo dnf install brave-browser -y
 
 # Preload deamon
-sudo dnf copr enable kylegospo/preload -y
-sudo dnf install preload -y
-sudo systemctl enable --now preload
+#sudo dnf copr enable kylegospo/preload -y
+#sudo dnf install preload -y
+#sudo systemctl enable --now preload
 
-# Install blender
-mkdir ~/blender-git
-mkdir ~/blender-git/lib
-cd ~/blender-git
-git clone https://git.blender.org/blender.git
-svn checkout https://svn.blender.org/svnroot/bf-blender/trunk/lib/linux_centos7_x86_64
-cd blender
-make update
-make
+# Install Blender
+mkdir -p {~/.local/bin,~/.local/share/applications/} && cd ~/.local/bin
+wget $blenderurl
+tar xpf ~/.local/bin/$blenderfile.tar.xz
+sudo ln -sf ~/.local/bin/$blenderfile/blender ~/.local/bin/blender
+cp ~/.local/bin/$blenderfile/blender.desktop ~/.local/share/applications/
 
 # Download Nordic Theme
 cd /usr/share/themes/
@@ -130,12 +138,21 @@ unzip FiraCode.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
 unzip Meslo.zip -d /home/$username/.fonts
 
-# Security
+# WIP - Security (firewalld already installed)
 sudo dnf install fail2ban -y
-sudo systemctl enable fail2ban && sudo systemctl start fail2ban
-#Edit /etc/fail2ban/jail.local
+#Edit /etc/fail2ban/jail.local to add log-paths ect.
 
 # Enable services
+sudo systemctl enable sddm
+sudo systemctl enable sshd
+sudo systemctl enable fail2ban
+sudo systemctl enable libvirtd
+sudo systemctl enable cups.service
+sudo systemctl enable NetworkManager
+sudo systemctl enable bluetooth
+
+usermod -aG libvirt $username
+sudo virsh net-autostart default
 
 
 # Reloading Fonts
